f45ec7bc5f8ef73e024cd9065176fc01c9f5142a
denis.tarasenko <denis.tarasenko@sbermarket.ru>
2023-07-13 17:29:59 +0300

    [DEX-1266] fix: rubocop

--- spec/internal/config/initializers/sentry.rb
+++ spec/internal/config/initializers/sentry.rb
@@ -9,7 +9,7 @@ Sentry.init do |config|
     def send_data(data)
       # for local sentry testing purposes
       Rails.logger.info("sending #{data.inspect} to Sentry")
-      sleep 1
+      sleep 1 # rubocop:disable Rails/CallingSleepInsideTests
     end
   end
   config.traces_sample_rate = 1.0
